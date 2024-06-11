a5b1ba43b4d7bb17258b20e5c3becf7310cfbcaf
Joe Whales <joewhales0@gmail.com>
2024-06-11 11:19:55 +0200

    updated cnn batch size and epochs

--- tutorial_5_CNN.py
+++ tutorial_5_CNN.py
@@ -11,8 +11,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 
 # Hyper-parameters
-num_epochs = 4
-batch_size = 4
+num_epochs = 10
+batch_size = 16
 learning_rate = 0.001
 
 # Transform
@@ -54,6 +54,32 @@ model = ConvNet().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+def evaluate_model(model, test_loader):
+    # Test the model
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        n_class_correct = [0 for i in range(10)]
+        n_class_samples = [0 for i in range(10)]
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            n_samples += labels.size(0)
+            n_correct += (predicted == labels).sum().item()
+            
+            for i in range(batch_size):
+                label = labels[i]
+                pred = predicted[i]
+                if (label == pred):
+                    n_class_correct[label] += 1
+                n_class_samples[label] += 1
+        
+        acc = 100.0 * n_correct / n_samples
+        print(f'Accuracy of the network: {acc} %')
+
+
 # Train the model
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
@@ -74,30 +100,8 @@ for epoch in range(num_epochs):
         if (i+1) % 100 == 0:
             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
-# Test the model
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    n_class_correct = [0 for i in range(10)]
-    n_class_samples = [0 for i in range(10)]
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
-        
-        for i in range(batch_size):
-            label = labels[i]
-            pred = predicted[i]
-            if (label == pred):
-                n_class_correct[label] += 1
-            n_class_samples[label] += 1
-    
-    acc = 100.0 * n_correct / n_samples
-    print(f'Accuracy of the network: {acc} %')
+    evaluate_model(model, test_loader)
 
-    for i in range(10):
-        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-        print(f'Accuracy of {classes[i]}: {acc} %')
+    # for i in range(10):
+    #     acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+    #     print(f'Accuracy of {classes[i]}: {acc} %')
